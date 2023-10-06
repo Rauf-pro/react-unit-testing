dbd6b9c22c365eb9b42e69b472473f01775ee7e8
rrauf8178 <rrauf8178@gmail.com>
2023-10-07 00:44:34 +0400

    feat/events-test: add test for events component

---
+++ src/components/Events/Events.test.js
@@ -0,0 +1,50 @@
+import { cleanup, render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Events from "./Events";
+afterEach(cleanup);
+
+describe("Events Component", () => {
+  /**
+   * getByText()
+   */
+  test("Should have a title", () => {
+    render(<Events />);
+    expect(screen.getByText(/Event Testing/i)).toBeInTheDocument();
+  });
+
+  /**
+   * getByText(): initial Value
+   */
+  test("Should have initial value 0", () => {
+    render(<Events />);
+    expect(screen.getByText(0)).toBeInTheDocument();
+  });
+
+  /**
+   * userEvent(): increment value
+   */
+  test("Should have increment value", async () => {
+    
+    render(<Events />);
+
+    const incrementBtn = screen.getByRole("button", {
+      name: /Increment/i,
+    });
+    await incrementBtn.click(incrementBtn);
+    expect(screen.getByText(1)).toBeInTheDocument();
+  });
+
+  /**
+   * userEvent(): decrement value
+   */
+
+  test("Should have decrement value", async () => {
+    render(<Events />);
+
+    const decrementBtn = screen.getByRole("button", {
+      name: /Decrement/i,
+    });
+    await decrementBtn.click(decrementBtn);
+    expect(screen.getByText(-1)).toBeInTheDocument();
+  });
+});
