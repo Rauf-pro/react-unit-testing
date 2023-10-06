1b4b565814a0277b5ca316a9b1d6d84c9cf01ce8
rrauf8178 <rrauf8178@gmail.com>
2023-10-07 00:11:43 +0400

    feat/events-component: create events component and import to app root

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Events from "./components/Events/Events";
 import Header from "./components/Header/Header";
 import Props from "./components/Props/Props";
 
@@ -9,7 +10,14 @@ function App() {
 
       <section className="main">
         {/** Props Testing */}
-        <Props header="Props Testing" description="Validating props in React !" />
+        <Props
+          header="Props Testing"
+          description="Validating props in React !"
+        />
+        <br />
+        <hr />
+        {/** Events Testing */}
+        <Events />
         <br />
         <hr />
       </section>
